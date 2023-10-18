fcd2cfcce8b49382c4ba8af6609760c5f6334cac
Yves Scherrer <yves.scherrer@gmail.com>
2023-10-18 11:45:08 +0300

    rnn-char

---
+++ models/rnn-char/train.sh
@@ -0,0 +1,31 @@
+#! /bin/bash -l
+
+#SBATCH -J rnn-archimob-skn
+#SBATCH -o rnn-archimob-skn.%j.out
+#SBATCH -e rnn-archimob-skn.%j.err
+#SBATCH --mem-per-cpu=8G
+#SBATCH -p gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -t 36:00:00
+
+module use -a /projappl/nlpl/software/modules/etc
+module load nlpl-mttools
+module load nlpl-opennmt-py
+
+for LG in archimob1c skn1c; do
+	echo $LG
+	sed 's/LG/'"$LG"'/g' < config_sentence.yaml > config_sent_"$LG".yaml
+	onmt_build_vocab -config config_sent_"$LG".yaml -n_sample -1
+	onmt_train -config config_sent_"$LG".yaml 2> $LG/train.log
+	BEST=`grep -Po 'Best model found at step \K(\d+)$' $LG/train.log`
+	if [ -z "$BEST" ];
+	then
+		BEST=`ls -1v $LG/model_step* | tail -n 1 | grep -Po '\d{2,}'`
+	fi
+	echo $BEST
+	cd $LG; ln -s model_step_"$BEST".pt model_best.pt; cd ../
+
+	onmt_translate -model $LG/model_best.pt -src ../data/$LG/dev.src -output $LG/dev_pred.tgt -replace_unk -max_length 1000 -min_length 1 -length_penalty wu -alpha 0.6
+done
