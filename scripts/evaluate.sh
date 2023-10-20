6a9a94de994c61fd0340e332ed3874064c56eb41
Yves Scherrer <yves.scherrer@gmail.com>
2023-10-20 11:23:24 +0300

    add evaluation scripts

---
+++ scripts/evaluate.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+echo "" > eval_results.txt
+
+MODEL=smt-char
+
+for LG in archimob1 gos1 skn1 ndc1 skn2 ndc2 skn3 ndc3; do
+    for SPLIT in a b c ; do
+        echo $LG $SPLIT
+        echo $LG $SPLIT >> eval_results.txt
+
+        # compute chrF, WER and CER
+        echo "chrF" >> eval_results.txt
+        sacrebleu ../processed_data/"$LG$SPLIT"/test.ref -m chrf < ../models/$MODEL/test_predictions/"$LG$SPLIT"/test_pred.tgt | jq -r .score >> eval_results.txt
+        echo "WER" >> eval_results.txt
+        python3 wer++.py ../models/$MODEL/test_predictions/"$LG$SPLIT"/test_pred.tgt ../processed_data/"$LG$SPLIT"/test.ref >> eval_results.txt
+        echo "CER" >> eval_results.txt
+        python3 wer++.py ../models/$MODEL/test_predictions/"$LG$SPLIT"/test_pred.tgt .../processed_data/"$LG$SPLIT"/test.ref >> eval_results.txt
+
+        # realign on word level
+        python3 align.py ../processed_data/"$LG$SPLIT"/dev.src ../models/$MODEL/test_predictions/"$LG$SPLIT"/test_pred.tgt ../data/"$LG$SPLIT"/dev.norm ../models/$MODEL/test_predictions/"$LG$SPLIT"/test_aligned.txt
+
+        # word-level evaluation with normEval.py
+        python3 normEval.py --gold ../data/"$LG$SPLIT"/dev.norm --pred ../models/$MODEL/test_predictions/"$LG$SPLIT"/test_aligned.txt >> eval_results.txt
+        echo "" >> eval_results.txt
+    done
+done
+
+
